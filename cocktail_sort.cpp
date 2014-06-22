85a9ac70f17319159ad799bb21c28a43c1a58478
zhihao li <lzhgus@gmail.com>
2014-06-21 20:23:23 -0400

    Update cocktail_sort.cpp

--- cocktail_sort.cpp
+++ cocktail_sort.cpp
@@ -3,6 +3,7 @@
   定向冒泡排序， 数组中的数字本是无序排放
   先找到最小的数放在第一位， 再找到最大的数放在最后一位
   再找第二小的数放在第二位， 然后第二大的数放在倒数第二位
+  时间复杂度n^2
 */
 
 #include <iostream>
@@ -11,18 +12,18 @@ using namespace std;
 void cocktail_sort(int[] a, int length)
 {
   for(int i = 0; i < length/2; i++){
-    //将最小的排到队尾
+    //将最小的排到队首
     for(int j = i; j < length - 1 - i; j++)
     {
-      if(a[j] < a[j+1]){
+      if(a[j] > a[j+1]){
         int temp = a[j];
         a[j] = a[j+1];
         a[j+1] = temp;
       }
     }
-    //将最大的排到队头
-    for(int j = length -(i+1) -1; j>i; j--){
-      if(a[j] > a[j-1]){
+    //将最大的排到队尾
+    for(int j = length -i -1; j>i; j--){
+      if(a[j] < a[j-1]){
         int temp = a[j];
         a[j] = a[j-1];
         a[j - 1] = temp;
@@ -31,3 +32,14 @@ void cocktail_sort(int[] a, int length)
   }
 }
 
+int main()
+{
+  int a[SIZE] = {1, 5, 3, 9, 6, 0 , 5, 2, 8};
+  int i;
+  bubble_sort(a, SIZE);
+  for(i = 0; i < SIZE; i++)
+  {
+    cout << a[i] << " ";
+  }
+  return 0;
+}
